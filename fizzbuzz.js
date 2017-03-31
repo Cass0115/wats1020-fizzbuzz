2635d6245a4842272eb14760610b5460261a5a96
Cassie Durr <Cass0115@gmail.com>
2017-03-30 18:18:09 -0700

    Fizz buzz update

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,26 @@
-// Place your FizzBuzz code here. 
+// Place your FizzBuzz code here.
+
+// Count through 1-20 and firgure out each on is divisible by 3, 5, or both.
+for (i=1; 1<=20; i++){
+  // For each number to the following check:
+
+  // Divisible by 3
+  if(i % 3 === 0){
+    // if so, also see if divisble by 5
+    if(i % 5 === 0){
+      // if divisible by 3 and 5; Print Fizzbuzz
+      console.log('FizzBuzz');
+    } else {
+      // if only divisible by 3; Print Fizz
+      console.log('Fizz');
+   }
+  } else if (1 % 5 === 0){
+    // Divisible by 5
+    // if so Print Buzz
+    console.log('Buzz');
+  } else{
+    // Not divisible 3 or 5
+    // if not Print Number
+    console.log(i);
+  }
+}
